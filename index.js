729e5b7c099d06947737fc2598688e02cd430578
Kit Langton <kit.langton@gmail.com>
2020-02-02 13:37:10 -0800

    Add async/await

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-import readline, { createInterface } from 'readline'
+import readline from 'readline-promise'
 
-const rl = createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-const beginGame = callback => {
-  rl.question('What word does your life depend on?\n', answer => {
-    console.log(`Your word is: "${answer}"`)
-    callback(answer)
-  })
+const beginGame = async () => {
+  const word = await rl.questionAsync('What word does your life depend on?\n')
+  console.log(`Your word is: "${word}"`)
+  playRound({ guesses: [], word })
 }
 
-beginGame(word => {
-  console.log('HANGMAN!')
-  playRound({ guesses: [], word })
-})
-
-const playRound = gameState => {
+const playRound = async gameState => {
   displayGuesses(gameState)
   displayWord(gameState)
   const winState = getWinState(gameState)
@@ -28,9 +22,8 @@ const playRound = gameState => {
     console.log('YOU LOSE!')
     rl.close()
   } else {
-    getGuess(gameState.guesses, guess => {
-      playRound({ ...gameState, guesses: [...gameState.guesses, guess] })
-    })
+    const guess = await getGuess(gameState.guesses)
+    playRound({ ...gameState, guesses: [...gameState.guesses, guess] })
   }
 }
 
@@ -44,19 +37,19 @@ const getWinState = gameState => {
   }
 }
 
-const getGuess = (guesses, callback) => {
-  rl.question('Enter a guess: ', guess => {
-    if (
-      /[a-z]/i.test(guess) &&
-      guess.length === 1 &&
-      guesses.indexOf(guess) === -1
-    ) {
-      callback(guess.toLowerCase())
-    } else {
-      console.log('Please input a valid guess.')
-      getGuess(guesses, callback)
-    }
-  })
+const getGuess = async guesses => {
+  const guess = await rl.questionAsync('Enter a guess: ')
+
+  if (
+    /[a-z]/i.test(guess) &&
+    guess.length === 1 &&
+    guesses.indexOf(guess) === -1
+  ) {
+    return guess.toLowerCase()
+  } else {
+    console.log('Please input a valid guess.')
+    return await getGuess(guesses)
+  }
 }
 
 const displayGuesses = gameState => {
